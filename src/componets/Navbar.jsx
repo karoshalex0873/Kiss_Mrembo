e75f93aff4a7f764d5d62484b60d36aaf6e934d3
Alex Karobia <akarobia30@gmail.com>
2024-06-22 17:26:59 +0300

    linking configuration

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
                     key={item}
                     href={`#${item.toLowerCase()}`}
                     onClick={() => scrollToSection(item.toLowerCase())}
-                    className="sm:text-white  text-darkBrand px-3 py-2 rounded-md text-lg font-medium"
+                    className="text-white px-3 py-2 rounded-md text-lg font-medium"
                     variants={navVariants}
                   >
                     {item}
